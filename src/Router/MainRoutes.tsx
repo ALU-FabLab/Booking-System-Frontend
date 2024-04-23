3147f20de84c31e4fb4a7bb3be183a9689355bc5
Phinatech <ugbojudithchinemerem@gmail.com>
2024-04-24 00:08:01 +0200

    pushing

--- src/Router/MainRoutes.tsx
+++ src/Router/MainRoutes.tsx
@@ -3,14 +3,25 @@ import { lazy } from "react";
 // import HomeLayout from "../layout/HomeLayout";
 import SigninLayout from "../layout/SigninLayout";
 import SignupLayout from "../layout/SignupLayout";
+import HomeLayout from "../layout/HomeLayout";
 
-// const HomeComp = lazy(() => import("../Pages/Homepage/"))
+const HomeComp = lazy(() => import("../Pages/Homepage/HomeComp"));
 const Signup = lazy(() => import("../Pages/Auth/Signup"));
 const Signin = lazy(() => import("../Pages/Auth/Signin"));
 
 export const element = createBrowserRouter([
   {
     path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomeComp />,
+      },
+    ],
+  },
+  {
+    path: "/signin",
     element: <SigninLayout />,
     children: [
       {
